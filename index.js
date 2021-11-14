ca7ce3205bf74544b3bfeb4d33d6893ee5a1b7a4
ShanjidAhmed <sjahmed116@gmail.com>
2021-11-14 17:48:06 +0600

    commit

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(cors());
 app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r7uyw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// console.log(uri)
 
 
 async function run() {
